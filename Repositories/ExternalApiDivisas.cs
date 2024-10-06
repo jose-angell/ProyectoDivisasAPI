1626ae729a1e18dd1d5bbf9904da94f4515d5357
jose Angel <josegallardo.cor@gmail.com>
2024-10-06 00:19:30 -0600

    proceso de error al consultar divisa

--- Repositories/ExternalApiDivisas.cs
+++ Repositories/ExternalApiDivisas.cs
@@ -12,20 +12,27 @@ namespace proyectoDivisas.Repositories
         }
         public async Task<Dictionary<string,float>> GetExternalData(string from, string to)
         {
-            var response = await _httpClient.GetAsync($"/latest?from={from}&to={to}");
-            response.EnsureSuccessStatusCode();
-            var divisa = await response.Content.ReadAsStringAsync();
-            var exchangeRates = JsonDocument.Parse(divisa);
-            var result = new Dictionary<string,float>();
-            if (exchangeRates.RootElement.TryGetProperty("rates", out JsonElement ratesElement) &&
-                       ratesElement.TryGetProperty(to, out JsonElement rateValue))
+            var result = new Dictionary<string, float>();
+            try
             {
-                result[to] = (float)rateValue.GetDouble();
-            }
-            else
+                var response = await _httpClient.GetAsync($"/latest?from={from}&to={to}");
+                response.EnsureSuccessStatusCode();
+                var divisa = await response.Content.ReadAsStringAsync();
+                var exchangeRates = JsonDocument.Parse(divisa);
+                if (exchangeRates.RootElement.TryGetProperty("rates", out JsonElement ratesElement) &&
+                           ratesElement.TryGetProperty(to, out JsonElement rateValue))
+                {
+                    result[to] = (float)rateValue.GetDouble();
+                }
+                else
+                {
+                    result[to] = 0f;
+                }
+            }catch (Exception ex)
             {
                 result[to] = 0f;
             }
+            
             return result;
         }
     }
